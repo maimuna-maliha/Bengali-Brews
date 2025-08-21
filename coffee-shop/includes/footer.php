8b2812e703da87cc0db9629f7606f9c1ca60a4ca
Mst. Maimuna Hossain Maliha <mst.maimuna.hossain.maliha@gmail.com>
2025-08-22 02:36:48 +0600

    Delete coffee-shop directory

--- coffee-shop/includes/footer.php
+++
@@ -1,8 +0,0 @@
-</main>
-<footer>
-    <p>Contact: info@bengalibrews.com | Phone: +880 1234-56789</p>
-    <p>&copy; <?php echo date('Y'); ?> Bengali Brews. All rights reserved.</p>
-</footer>
-<script src="<?php echo $base_path; ?>assets/js/script.js"></script>
-</body>
-</html>
